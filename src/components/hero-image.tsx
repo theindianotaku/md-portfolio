08ae4e97114d984fc5bc994638e08b2d89238d2a
Debashish Nayak <debashishzero@gmail.com>
2025-05-03 12:50:53 +0000

    feat(HeroImage): fix image CLS issue

--- src/components/hero-image.tsx
+++ src/components/hero-image.tsx
@@ -1,39 +1,18 @@
 import Image from 'next/image';
 
-import desktopPNG from '@/assets/images/hero.png';
 import desktopWebP from '@/assets/images/hero.webp';
-import mobilePNG from '@/assets/images/hero-mobile.png';
-import mobileWebP from '@/assets/images/hero-mobile.webp';
 
 const HeroImage = () => {
   return (
     <div className="flex items-center justify-center max-w-[300px] md:flex-[3] md:max-w-[400px]">
-      <div className="md:hidden">
-        <picture>
-          <source srcSet={mobileWebP.src} type="image/webp" />
-          <Image
-            src={mobilePNG}
-            alt="Debashish's photo"
-            priority={true}
-            width={mobilePNG.width}
-            height={mobilePNG.height}
-            className="w-full h-auto"
-          />
-        </picture>
-      </div>
-      <div className="hidden md:block">
-        <picture>
-          <source srcSet={desktopWebP.src} type="image/webp" />
-          <Image
-            src={desktopPNG}
-            alt="Debashish's photo"
-            priority={true}
-            width={desktopPNG.width}
-            height={desktopPNG.height}
-            className="w-full h-auto"
-          />
-        </picture>
-      </div>
+      <Image
+        src={desktopWebP}
+        alt="Debashish's photo"
+        width={desktopWebP.width}
+        height={desktopWebP.height}
+        className="object-cover"
+        sizes="(min-width: 768px) 37.5vw, 90vw"
+      />
     </div>
   );
 };
